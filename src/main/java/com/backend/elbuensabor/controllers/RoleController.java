9c22c60041e2a1ebb06ed68e62f059b4763dc2c0
MatiasBarreto93 <barretomatias93@gmail.com>
2023-05-06 20:48:08 -0300

    Revision Mapeo V2 (OK 100%) Se comento la depedencia "0auth-2" Se completo lo necesario para el funcionamiento generico Prueba de API con Postman (OK 100%)

---
+++ src/main/java/com/backend/elbuensabor/controllers/RoleController.java
@@ -0,0 +1,28 @@
+package com.backend.elbuensabor.controllers;
+
+import com.backend.elbuensabor.controllers.impl.GenericControllerImpl;
+import com.backend.elbuensabor.entities.Role;
+import com.backend.elbuensabor.services.impl.RoleServiceImpl;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "*")
+@RequestMapping(path = "api/v1/roles")
+public class RoleController extends GenericControllerImpl<Role, RoleServiceImpl> {
+    //Get All @GET
+    //http://localhost:8080/api/v1/roles
+
+    //Get One @GET
+    //http://localhost:8080/api/v1/roles/id
+
+    //Create @POST
+    //http://localhost:8080/api/v1/roles + JSON
+
+    //Update @PUT
+    //http://localhost:8080/api/v1/roles/id + JSON
+
+    //Delete @DELETE
+    //http://localhost:8080/api/v1/roles/id
+}
