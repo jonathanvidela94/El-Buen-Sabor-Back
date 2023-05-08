047c05240bbf2f27a8b335a44c04d582e3c73000
MatiasBarreto93 <barretomatias93@gmail.com>
2023-05-08 20:32:19 -0300

    Auth0 Funcionando

--- src/main/java/com/backend/elbuensabor/configuration/security/SecurityConfiguration.java
+++ src/main/java/com/backend/elbuensabor/configuration/security/SecurityConfiguration.java
@@ -8,7 +8,6 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
 import org.springframework.security.oauth2.core.OAuth2TokenValidator;
 import org.springframework.security.oauth2.jwt.*;
@@ -87,10 +86,4 @@ public class SecurityConfiguration {
         jwtConverter.setJwtGrantedAuthoritiesConverter(converter);
         return jwtConverter;
     }
-
-    @Bean
-    public WebSecurityCustomizer webSecurityCustomizer() {
-        return web -> web.ignoring().anyRequest();
-    }
-
 }
