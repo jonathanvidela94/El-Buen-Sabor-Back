2a02bf70a80535ffeeef350c8d01f9a51b20cdcf
MatiasBarreto93 <barretomatias93@gmail.com>
2023-05-13 12:40:55 -0300

    ENV Working

--- src/main/java/com/backend/elbuensabor/controllers/RoleController.java
+++ src/main/java/com/backend/elbuensabor/controllers/RoleController.java
@@ -22,8 +22,6 @@ public class RoleController extends GenericControllerImpl<Role, RoleServiceImpl>
         return super.getAll();
     }
 
-    //Test
-
     //Get One @GET
     //http://localhost:8080/api/v1/roles/id
 
