53f6fcfc05cffc1ad5caff7be5ac7e3f0b109bdd
MatiasBarreto93 <barretomatias93@gmail.com>
2023-05-20 23:41:30 -0300

    Usuarios 110%

--- src/main/java/com/backend/elbuensabor/controllers/UserController.java
+++ src/main/java/com/backend/elbuensabor/controllers/UserController.java
@@ -3,14 +3,17 @@ package com.backend.elbuensabor.controllers;
 import com.backend.elbuensabor.controllers.impl.GenericControllerImpl;
 import com.backend.elbuensabor.entities.User;
 import com.backend.elbuensabor.services.impl.UserServiceImpl;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin(origins = "*")
 @RequestMapping(path = "api/v1/user")
 public class UserController extends GenericControllerImpl<User, UserServiceImpl> {
+
+    private static final String ERROR_MESSAGE = "{\"error\":\"Error. Por favor intente nuevamente.\"}";
+
 
     //Get All @GET
     //http://localhost:8080/api/v1/user
@@ -27,5 +30,15 @@ public class UserController extends GenericControllerImpl<User, UserServiceImpl>
     //Delete @DELETE
     //http://localhost:8080/api/v1/user/id
 
+
+    @PutMapping("/{id}/block")
+    public ResponseEntity<?> bloquearDesbloquearEmpleado(@PathVariable Long id, @RequestParam boolean blocked) {
+        try {
+            User user = service.bloquearDesbloquearEmpleado(id, blocked);
+            return ResponseEntity.status(HttpStatus.OK).body(user);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ERROR_MESSAGE);
+        }
+    }
 }
 
