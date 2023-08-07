cf18bbb97334df86028db003e6528e3e1684ed38
jonathanvidela94 <JohnnyGc11@gmail.com>
2023-08-07 10:54:35 -0300

    limpliando

--- src/main/java/com/backend/elbuensabor/DTO/ItemTypeDTO.java
+++
@@ -1,8 +0,0 @@
-package com.backend.elbuensabor.DTO;
-
-import lombok.Data;
-
-@Data
-public class ItemTypeDTO extends GenericDTO{
-    private String denomination;
-}
--- src/main/java/com/backend/elbuensabor/DTO/RoleDTO.java
+++
@@ -1,9 +0,0 @@
-package com.backend.elbuensabor.DTO;
-
-import lombok.Data;
-
-@Data
-public class RoleDTO extends GenericDTO{
-    private String denomination;
-    private String auth0RolId;
-}
